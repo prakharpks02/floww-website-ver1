ee6785ce4bc715227ae6e28afc63073e6fe679de
Prakhar <prakharpks02@gmail.com>
2022-06-05 19:28:22 +0530

    api remaining to connect

---
+++ js/delivery-vendor-management.js
@@ -0,0 +1,134 @@
+var orderList = [{
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+}, {
+    'orderId': 'ORD0000005',
+    'dropAddress': 'Andheri East, Mumbai',
+    'dropContact': '9910530300',
+    'pickAddress': 'Andheri East, Mumbai',
+    'pickContact': '9910530300',
+    'weight': '20',
+    'instruction': 'Leave near watchman',
+    'status': 'delivered',
+},
+];
+
+Vue.component('order-row-component', {
+    props: {
+        'orderdata': Object,
+    },
+    delimiters: ['[[', ']]'],
+    template: `
+				<div class="order-list-item box columns is-mobile is-vcentered">
+					<div class="column">
+                        <p class="help">[[orderdata.orderId]]</p>
+                        <p class="">[[orderdata.pickContact]]</p>
+                    </div>
+                    <div class="column has-text-right">
+                        <span class="tag">[[orderdata.status]]</span>
+                        <button class="button is-small"  @click="$emit('view-order')">View Order</button>
+                    </div>
+				</div>
+			`,
+})
+
+
+var app1 = new Vue({
+    el: '#main-page-id',
+    data: {
+        'orderList': orderList,
+        'orderDetail': '',
+        'viewOrderStatus':false,
+    },
+    methods: {
+        ViewOrder: function (orderid, indexno) {
+            console.log(orderid);
+            this.orderDetail = this.orderList[indexno];
+            this.viewOrderStatus = true;
+        },
+        DownloadBill: function (taskid) {
+            console.log(taskid);
+            window.alert("Coming Soon");
+        },
+        OrderDelivered: function (orderid) {
+            console.log(orderid);
+            window.alert("Coming Soon");
+        },
+    },
+    mounted() {
+        // axios.get(globalApiUrl + '/api/v1/search/get-order-list/', {
+        //     params: {
+        //         vendorId: queryVendorId,
+        //     }
+        // })
+        //     .then(function (response) {
+        //         let responseData = JSON.parse(response.data);
+        //         console.log(responseData);
+        //         app1.orderList = responseData;
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //         window.alert('Server Error, please try again later');
+        //     });
+    },
+})
