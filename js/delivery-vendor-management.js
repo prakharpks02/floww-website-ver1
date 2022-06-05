2716faa6c7e4293986f27279955aa3ffed759000
Prakhar <prakharpks02@gmail.com>
2022-06-05 20:38:48 +0530

    delivery vendor mgt updated api connected

--- js/delivery-vendor-management.js
+++ js/delivery-vendor-management.js
@@ -82,7 +82,7 @@ Vue.component('order-row-component', {
 				<div class="order-list-item box columns is-mobile is-vcentered">
 					<div class="column">
                         <p class="help">[[orderdata.orderId]]</p>
-                        <p class="">[[orderdata.pickContact]]</p>
+                        <p class="">[[orderdata.pickupContactNo]]</p>
                     </div>
                     <div class="column has-text-right">
                         <span class="tag">[[orderdata.status]]</span>
@@ -97,38 +97,53 @@ var app1 = new Vue({
     el: '#main-page-id',
     data: {
         'orderList': orderList,
-        'orderDetail': '',
-        'viewOrderStatus':false,
+        'orderDetail': {},
+        'viewOrderStatus': false,
+        'currentIndex': 0,
     },
     methods: {
         ViewOrder: function (orderid, indexno) {
             console.log(orderid);
             this.orderDetail = this.orderList[indexno];
             this.viewOrderStatus = true;
+            this.currentIndex = indexno;
         },
         DownloadBill: function (taskid) {
             console.log(taskid);
-            window.alert("Coming Soon");
+            window.open("https://backend.gofloww.co/api/v1/documents/receipt/" + taskid, '_blank');
         },
         OrderDelivered: function (orderid) {
             console.log(orderid);
-            window.alert("Coming Soon");
+            axios.post(globalApiUrl + '/api/v1/search/update-order-status/', {
+                orderId: orderid,
+                status: 'delivered',
+            })
+                .then(function (response) {
+                    let responseData = JSON.parse(response.data);
+                    console.log(responseData);
+                    app1.orderDetail.status = "delivered";
+                    app1.orderList[app1.currentIndex].status = "delivered";
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    window.alert('Server Error, Please Try Again!');
+                });
         },
     },
     mounted() {
-        // axios.get(globalApiUrl + '/api/v1/search/get-order-list/', {
-        //     params: {
-        //         vendorId: queryVendorId,
-        //     }
-        // })
-        //     .then(function (response) {
-        //         let responseData = JSON.parse(response.data);
-        //         console.log(responseData);
-        //         app1.orderList = responseData;
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //         window.alert('Server Error, please try again later');
-        //     });
+        axios.get(globalApiUrl + '/api/v1/3pl-form/get-delivery-vendor-order-list/', {
+            params: {
+                vendorId: queryVendorId,
+            }
+        })
+            .then(function (response) {
+                let responseData = JSON.parse(response.data);
+                console.log(responseData);
+                app1.orderList = responseData;
+            })
+            .catch(function (error) {
+                console.log(error);
+                window.alert('Server Error, please try again later');
+            });
     },
 })
